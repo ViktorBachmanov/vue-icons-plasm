e705ee03881a4be24648d329274cd356d22bc660
Viktor Bachmanov <vbachmanov@mail.ru>
2024-06-02 10:01:04 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import IconTelegram from './components/IconTelegram.vue'
+</script>
+
+<template>
+  <div>
+    <IconTelegram style="color: magenta" />
+  </div>
+</template>
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import IconTelegram from "./components/IconTelegram.vue";
+
+export { IconTelegram };
